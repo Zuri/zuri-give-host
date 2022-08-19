e759e6795efae2ac20042a53b093a6409e7ef0ec
deronhurst <deronjkhurst@gmail.com>
2022-08-19 12:49:47 -0600

    some style updates

--- src/components/DonationModal.vue
+++ src/components/DonationModal.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="zuri-give">
+    <div id="tsparticles"></div>
     <div class="modal fade" ref="donationModal" id="donationModal" tabindex="-1" aria-hidden="true">
       <!-- Modal & IFrame -->
       <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content bg-transparent border-0">
           <div class="modal-body">
-            <div class="bg-primary rounded-circle d-flex p-2" style="position: absolute; right: -0.25rem; top: -0.25rem">
+            <div class="bg-dark rounded-circle d-flex p-2" style="position: absolute; right: -0.75rem; top: -0.75rem">
               <button type="button" class="btn-close btn-close-white m-auto bg-transparent" data-bs-dismiss="modal" aria-label="Close" />
             </div>
             <iframe
@@ -15,10 +16,15 @@
               title="Donation Widget"
             ></iframe>
           </div>
-          <div class="modal-footer bg-transparent border-0 d-block text-center text-white">
+          <div class="d-none modal-footer bg-transparent border-0 d-block text-center text-white">
             <p class="fs-5">
               AICF custom message goes here.
-              <span class="fs-6 fw-light d-block">This demo is currently under development 💻</span>
+              <span class="fs-6 fw-light d-flex justify-content-between">
+                <address>Greenwood Blvd. Denver, CO 80221</address>
+                <a class="text-white" href="tel:303-426-8900">303-426-8900</a>
+                <a class="text-white" href="tel:800-776-FUND">800-776-FUND</a>
+                <a class="text-white" href="mailto:info@collegefund.org">info@collegefund.org</a>
+              </span>
             </p>
           </div>
         </div>
--- src/helpers/modal.js
+++ src/helpers/modal.js
@@ -1,8 +1,40 @@
-import { Modal } from "bootstrap"
 import { Toast } from "bootstrap"
+import { tsParticles } from "tsparticles-engine"
+import { loadConfettiPreset } from "tsparticles-preset-confetti"
+import { loadImageShape } from "tsparticles-shape-image"
 
 let modal = null
+let resizer = null
 let complete = false
+
+export const initModal = (el) => {
+  modal = el
+
+  initOpeners()
+  initClosers()
+  initResizer()
+  preloadConfetti()
+}
+
+const initOpeners = () => {
+  modal._element.addEventListener('shown.bs.modal', function(event) {
+    window.dispatchEvent(new Event('resize'))
+  })
+
+  document.querySelectorAll(".widget-opener").forEach((el) => {
+    el.addEventListener("click", () => {
+      hideOpeners()
+      hideToast(donationWidgetToastFail)
+      modal.show()
+      // document.getElementById('donationIframe').resize()
+      // Force a resize 
+      // resizer.resize()
+      // setTimeout(() => {
+      //   window.dispatchEvent(new Event('resize'))
+      // }, 500)
+    })
+  })
+}
 
 const hideOpeners = () => {
   document.querySelectorAll('.widget-opener').forEach(el => {
@@ -39,31 +71,14 @@ const showToast = (toastElement) => {
   }, 500)
 }
 
-const initOpeners = () => {
-  document.querySelectorAll(".widget-opener").forEach((el) => {
-    // el.classList.remove("fade")
-    el.addEventListener("click", () => {
-      hideOpeners()
-      hideToast(donationWidgetToastFail)
-      modal.show()
-    })
-  })
-}
-
 const initClosers = () => {
   modal._element.addEventListener("hidden.bs.modal", (e) => {
     if (!complete) {
-      // donationWidgetOpener.classList.remove("d-none")
-
       showToast(donationWidgetToastFail)
       showOpeners()
     } else {
       // reset form
-      document
-        .getElementById("donationIframe")
-        .contentDocument.location.reload(true)
-
-      showToast(donationWidgetToastSuccess)
+      document.getElementById("donationIframe").contentDocument.location.reload(true)
     }
   })
 }
@@ -75,58 +90,192 @@ const updateAmount = (amount) => {
 }
 
 const doThankYou = () => {
-  setTimeout(() => {
-    confetti({
-      particleCount: 100,
-      spread: 90,
-      gravity: 0.5,
-      zIndex: 9999999,
-      origin: {
-        x: 0.5,
-        y: 0.4126,
+  const tsParticlesConfig = {
+    "fullScreen": {
+      "zIndex": 99999
+    },
+    "emitters": {
+      "startCount": 30,
+      "position": {
+        "x": 50,
+        "y": 100
       },
-    })
-  }, 1500)
+      "size": {
+        "width": 0,
+        "height": 0
+      },
+      "rate": {
+        "delay": 0,
+        "quantity": .1
+      },
+      "life": {
+        "duration": .5,
+        "count": 1.5
+      }
+    },
+    "particles": {
+      "move": {
+        "decay": 0.01,
+        "direction": "top",
+        "enable": true,
+        "gravity": {
+          "enable": true,
+          "acceleration": 2,
+        },
+        "outModes": {
+          "top": "none",
+          "default": "destroy"
+        },
+        "speed": {
+          "min": 20,
+          "max": 50
+        }
+      },
+      "number": {
+        "value": 0
+      },
+      "opacity": {
+        "value": 1
+      },
+      "rotate": {
+        "value": {
+          "min": 0,
+          "max": 360
+        },
+        "direction": "random",
+        "animation": {
+          "enable": true,
+          "speed": 15
+        }
+      },
+      "size": {
+        "value": 3,
+        "animation": {
+          "enable": true,
+          "startValue": "min",
+          "count": 1,
+          "speed": 16,
+          "sync": true
+        }
+      },
+      "wobble": {
+        "distance": 30,
+        "enable": true,
+        "speed": {
+          "min": -7,
+          "max": 7
+        }
+      },
+      "shape": {
+        "type": "image",
+        "options": {
+          "image": [
+            {
+              "src": "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10065/graduation-cap-smiley.png?v=1660152921000",
+              "width": 64,
+              "height": 62,
+              "particles": {
+                "size": {
+                  "value": 32
+                }
+              }
+            },
+            {
+              "src": "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10065/graduation-cap_01.png?v=1660152918000",
+              "width": 195,
+              "height": 117,
+              "particles": {
+                "size": {
+                  "value": 32
+                }
+              }
+            },
+            {
+              "src": "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10065/graduation-cap_02.png?v=1660152919000",
+              "width": 211,
+              "height": 110,
+              "particles": {
+                "size": {
+                  "value": 36
+                }
+              }
+            },
+            {
+              "src": "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10065/graduation-cap_03.png?v=1660152920000",
+              "width": 218,
+              "height": 133,
+              "particles": {
+                "size": {
+                  "value": 38
+                }
+              }
+            },
+            {
+              "src": "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10065/graduation-cap_04.png?v=1660152920000",
+              "width": 196,
+              "height": 115,
+              "particles": {
+                "size": {
+                  "value": 32
+                }
+              }
+            },
+          ]
+        }
+      }
+    },
+    // "responsive": [
+    //   {
+    //     "maxWidth": 1024,
+    //     "options": {
+    //       "particles": {
+    //         "move": {
+    //           "speed": {
+    //             "min": 33,
+    //             "max": 66
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // ]
+  }
+  setTimeout(() => {
+    tsParticles.load("tsparticles", tsParticlesConfig)
+  }, 1000)
 }
 
 const initResizer = () => {
-  // console.log("INIT RESIZER")
-  iFrameResize(
-    {
-      log: false,
-      checkOrigin: false,
-      onMessage: ({ iframe, message }) => {
-        switch (message.type || message) {
-          case "ready":
-            showOpeners()
-            document.querySelectorAll(".widget-opener.fade").forEach((el) => {
-              el.classList.add("show")
-            })
-            break
-          case "amount change":
-            updateAmount(message.value)
-            break
-          case "cancel":
-            modal.hide()
-            break
-          case "complete":
-            complete = true
-            modal.hide()
-            break
-          case "thank you":
-            doThankYou()
-            break
-        }
-      },
+  iFrameResize({
+    log: false,
+    checkOrigin: false,
+    onMessage: ({ iframe, message }) => {
+      switch (message.type || message) {
+        case "ready":
+          showOpeners()
+          document.querySelectorAll(".widget-opener.fade").forEach((el) => {
+            el.classList.add("show")
+          })
+          break
+        case "amount change":
+          updateAmount(message.value)
+          break
+        case "thank you":
+          complete = true
+          setTimeout(() => {
+            hideOpeners()
+          }, 1000)
+          doThankYou()
+          break
+      }
     },
+  },
     "#donationIframe"
   )
 }
 
-export const initModal = (el) => {
-  modal = el
-
-  initOpeners()
-  initClosers()
-  initResizer()
+const preloadConfetti = () => {
+  document.body.appendChild(document.getElementById("tsparticles"))
+  loadConfettiPreset(tsParticles)
+  loadImageShape(tsParticles)
 }
